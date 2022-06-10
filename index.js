4acf79a8013345b1d9f268a30c7a89dd6620df1a
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-06-10 13:30:10 -0400

    commit tree ed5e39a74e47fe4ed811d7fad7802619f96bcb93

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+function findMatching(driver, name){
+    let newArray = [];
+    for(let i = 0; i < driver.length; i++){
+        if(driver[i].toLowerCase() === name.toLowerCase()){
+            newArray.push(driver[i]);
+        }
+    }
+    return newArray;
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(function(letters){
+        if(letters.toLowerCase().indexOf(name.toLowerCase()) !== -1){
+            for(let i = 0; i < letters.length; i++){
+                if(letters.toLowerCase().indexOf(name.toLowerCase()) >= i){
+                    return letters.toLowerCase().indexOf(name.toLowerCase()) === i;
+                }
+
+            }
+        }
+    })
+}
+
+function matchName(driver, name){
+    let newArray = [];
+    for(let i = 0; i < driver.length; i++){
+        if(driver[i].name === name){
+            newArray.push(driver[i]);
+        }
+    }
+    return newArray;
+}
+
